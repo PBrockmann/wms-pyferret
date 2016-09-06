53a5ddb0def48dc74fe1a425ad7ca6ab564a5ca6
Patrick Brockmann <Patrick.Brockmann@lsce.ipsl.fr>
2016-09-06 19:56:40 +0200

    Add examples in the shell

--- slippy_map.bash
+++ slippy_map.bash
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+#-------------------------------------
+# Example: 
+# ./slippy_map.bash 'shade/lev=(-inf)(-10,30,1)(inf)/pal=mpl_PSU_viridis temp[k=@max]'
+# ./slippy_map.bash 'shade/lev=(-inf)(30,40,1)(inf)/pal=mpl_PSU_inferno salt[k=1]'
+
+#-------------------------------------
 # launch NodeWebkit with slippy_map.html (package.json)
 nw . &
 
+#-------------------------------------
 # launch gunicorn to render tiles from pyferret
-#gunicorn -w 4 --env CMD='shade/lev=(-inf)(-10,30,1)(inf)/pal=mpl_PSU_plasma temp[k=@var]' myapp_request_pyferret_01:app
 gunicorn -w 4 --env CMD="$1" myapp_request_pyferret_01:app
