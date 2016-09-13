44eb772e39629cf0de9427c4498bc5c47e395a07
Patrick Brockmann <Patrick.Brockmann@lsce.ipsl.fr>
2016-09-13 15:44:27 +0200

    Update pyferretWMS_test.py

--- pyferretWMS_test.py
+++ pyferretWMS_test.py
@@ -1,5 +1,5 @@
 
-# python test.py 'shade/lev=(-inf)(-10,30,1)(inf)/pal=mpl_PSU_plasma temp[k=@max]'
+# python pyferretWMS_test.py 'shade/lev=(-inf)(-10,30,1)(inf)/pal=mpl_PSU_plasma temp[k=@max]'
 
 import sys
 import pyferret, pyferretWMS
