dc330126393cbb697c054719d3a5732b6570dd12
PBrockmann <Patrick.Brockmann@lsce.ipsl.fr>
2016-09-20 20:26:49 +0200

    remove tmpdir

--- pyferretWMS.py
+++ pyferretWMS.py
@@ -76,7 +76,7 @@ class myArbiter(gunicorn.arbiter.Arbiter):
         pyferret.stop()
 
 	print('Removing temporary directory: ', tmpdir)
-	#shutil.rmtree(tmpdir)
+	shutil.rmtree(tmpdir)
 
         super(myArbiter, self).halt()
 
